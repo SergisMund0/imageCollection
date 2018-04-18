4bda11c4cb4e6b5c1a4287ef0f6f04e140190b50
Sergio Garre Ramon <sergio.garre.ramon@everis.com>
2018-04-18 17:29:02 +0200

    Initial Commit

---
+++ imageCollection/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  imageCollection
+//
+//  Created by Sergio Garre Ramon on 18/04/2018.
+//  Copyright © 2018 IndividualUser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
