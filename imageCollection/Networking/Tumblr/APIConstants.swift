f90d4b25ad75c9e53d0e6eef8ede7792c4747f46
Sergio Garre Ramon <sergio.garre.ramon@everis.com>
2018-04-18 23:55:41 +0200

    Constants for Tumblr API added

---
+++ imageCollection/Networking/Tumblr/APIConstants.swift
@@ -0,0 +1,13 @@
+//
+//  APIConstants.swift
+//  imageCollection
+//
+//  Created by Sergio Garre Ramon on 18/04/2018.
+//  Copyright © 2018 IndividualUser. All rights reserved.
+//
+
+import Foundation
+
+enum APIConstants: String {
+    case baseURL = "https://api.tumblr.com/v2"
+}
