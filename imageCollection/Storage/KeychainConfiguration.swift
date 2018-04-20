7e2830ca7d44cf27fbbe93adb25f8c1fc46ea03f
Sergio Garre Ramon <sergio.garre.ramon@everis.com>
2018-04-20 22:56:48 +0200

    Uploaded Keychain configuration in order to store OAuth credentials session. Uploaded Apple's own Keychain wrapper; GenericKeychain; KeychainPasswordItem.

---
+++ imageCollection/Storage/KeychainConfiguration.swift
@@ -0,0 +1,14 @@
+//
+//  KeychainConfiguration.swift
+//  imageCollection
+//
+//  Created by Sergio Garre Ramon on 20/04/2018.
+//  Copyright © 2018 IndividualUser. All rights reserved.
+//
+
+import Foundation
+
+/// Keychain configuration; There is no access group by the moment because we do not want to share information to another applications.
+struct KeychainConfiguration {
+    static let serviceName = "imageCollection"
+}
