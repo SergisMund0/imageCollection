8fa7494bf013064b164f869fab4a13bbd437d95e
Sergio Garre Ramon <sergio.garre.ramon@everis.com>
2018-04-21 01:10:43 +0200

    Updated ImageCollectionCell layout. Updated ImageCollectionTableView layout. Made layout-constraints to show the content correctly.

---
+++ imageCollection/Components/ImageCollectionCell.swift
@@ -0,0 +1,15 @@
+//
+//  ImageCollectionCell.swift
+//  imageCollection
+//
+//  Created by Sergio Garre Ramon on 19/04/2018.
+//  Copyright © 2018 IndividualUser. All rights reserved.
+//
+
+import UIKit
+
+class ImageCollectionCell: UITableViewCell {
+    @IBOutlet weak var tagLabelView: UILabel!
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var dateLabelView: UILabel!
+}
